c2c6e3ff1198fa0b311931e502bc3137161ddbf1
sven <svenrisse0@gmail.com>
2022-11-12 10:32:53 +0100

    Changed to js, added functionality to add a dynamically rendered todo

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+    import {nanoid} from "nanoid"
+
+
+    export default {
+        data() {
+            return {
+                todo: {title: "", description: ""},
+                todos: []
+            }
+        },
+
+        methods: {
+            addToDo() {
+                this.todos.push(this.todo)
+                this.todo = {title: "", description: ""}
+            },
+            removeToDo() {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <form @submit.prevent="addToDo">
+        <label for="title">Title</label>
+        <input type="text" name="title" v-model="todo.title">
+        <label for="description">Description</label>
+        <input type="text" name="description" v-model="todo.description">
+        <button>Add To Do</button>
+    </form>
+
+    <div v-for="todo in todos">
+      <div :id="todos.indexOf(todo)">
+        <div>Title: {{todo.title }}</div>
+        <div>Description: {{todo.description}}</div>
+        <button @click="removeToDo">Remove</button>
+      </div>
+    </div>
+</template>
