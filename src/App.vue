57ac93f2acbbfae171e7827c13bcf618a31c9729
sven <svenrisse0@gmail.com>
2022-11-15 11:42:28 +0100

    refactor: added classes and removed "labels"

--- src/App.vue
+++ src/App.vue
@@ -21,49 +21,62 @@ function addToDo() {
 }
 
 function removeToDo(e) {
-    const id = e.target.getAttribute("id")
-    todos.value.splice(id, 1)
+    const id = e.target.getAttribute("id");
+    todos.value.splice(id, 1);
 }
 
 onMounted(() => {
     if (localStorage.getItem("todos")) {
-        todos.value = JSON.parse(localStorage.getItem("todos"))
+        todos.value = JSON.parse(localStorage.getItem("todos"));
     }
-})
+});
 
-watch(todos, (newVal) => {
-    console.log("Watch executed")
-    localStorage.setItem("todos", JSON.stringify(newVal))
-}, {
-    deep: true
-})
-
+watch(
+    todos,
+    (newVal) => {
+        console.log("Watch executed");
+        localStorage.setItem("todos", JSON.stringify(newVal));
+    },
+    {
+        deep: true,
+    }
+);
 </script>
 
 <template>
-    <form @submit.prevent="addToDo">
+    <form @submit.prevent="addToDo" class="add-section">
         <input
             type="text"
             name="title"
             v-model="todo.title.value"
             placeholder="Title"
+            class="add-section__input"
+            required
         />
         <input
             type="text"
             name="description"
             v-model="todo.description.value"
             placeholder="Description"
+            class="add-section__input"
         />
-        <button>+</button>
+        <button class="add-section__button">+</button>
     </form>
-
-    <div v-for="task in todos">
-        <div>
-            <div>Title: {{ task.title }}</div>
-            <div v-if="task.description">
-                Description: {{ task.description }}
+    <div class="task-section">
+        <div v-for="task in todos" class="task-section__task">
+            <div class="task-section__text">
+                <div class="task-section__title">{{ task.title }}</div>
+                <div v-if="task.description" class="task-section__desc">
+                    {{ task.description }}
+                </div>
             </div>
-            <button @click="removeToDo" :id="todos.indexOf(task)">-</button>
+            <button
+                @click="removeToDo"
+                :id="todos.indexOf(task)"
+                class="task-section__button"
+            >
+                Remove
+            </button>
         </div>
     </div>
 </template>
