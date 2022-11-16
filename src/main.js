cb8afe0eff92f43a4dc00b3140d611ef8cfa53cf
sven <svenrisse0@gmail.com>
2022-11-16 22:21:20 +0100

    feat: added register

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
+import router from "./router"
+import firebase from "firebase/compat/app"
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+    apiKey: "AIzaSyCW2pImbEEBPb5h8IbHF-xq5l9pxcqY3mw",
+    authDomain: "vue-to-do-81c7d.firebaseapp.com",
+    projectId: "vue-to-do-81c7d",
+    storageBucket: "vue-to-do-81c7d.appspot.com",
+    messagingSenderId: "565574310353",
+    appId: "1:565574310353:web:57cf83afb8cec29b3268f0",
+    measurementId: "G-F9XS93SV16",
+}
+
+firebase.initializeApp(firebaseConfig)
+
+createApp(App).use(router).mount("#app")
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,1 +1,30 @@
-<template></template>
+<script setup>
+import { ref } from "vue"
+import firebase from "firebase/compat"
+import { useRouter } from "vue-router"
+
+const email = ref("")
+const password = ref("")
+const router = useRouter()
+
+function register() {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email.value, password.value)
+        .then((data) => {
+            console.log("Successfully registered!")
+            router.push("/todos")
+        })
+        .catch(error => {
+            console.log(error.code)
+            alert(error.message)
+        })
+}
+</script>
+
+<template>
+    <h1>Create an Account</h1>
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+    <p><button @click="register">Submit</button></p>
+</template>
